52215be6f5c39a28672764366718bb8e1e8000f7
Skati <drozdovsv87@gmail.com>
2020-04-21 17:33:32 +0300

    max_alpha added

--- reflex.py
+++ reflex.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import os
 import xlsxwriter
+from openpyxl import load_workbook
 
 total_xls_path = './excel/total.xlsx'
 files_dir = './data/'
@@ -10,11 +11,11 @@ lst_files = os.listdir(files_dir)
 
 def write_xls(file_path):
     # headers for df
-    headers1 = ['f', 'alfa_75', 'Y1_75', 'R1_75']
-    headers2 = ['alfa_80', 'Y1_80', 'R1_80']
-    headers3 = ['alfa_85', 'Y1_85', 'R1_85']
-    headers4 = ['alfa_90', 'Y1_90', 'R1_90']
-    headers5 = ['alfa_95', 'Y1_95', 'R1_95']
+    headers1 = ['f', 'alpha_75', 'Y1_75', 'R1_75']
+    headers2 = ['alpha_80', 'Y1_80', 'R1_80']
+    headers3 = ['alpha_85', 'Y1_85', 'R1_85']
+    headers4 = ['alpha_90', 'Y1_90', 'R1_90']
+    headers5 = ['alpha_95', 'Y1_95', 'R1_95']
     headers = headers1+headers2+headers3+headers4+headers5
     writer = pd.ExcelWriter(file_path)
     workbook = writer.book
@@ -34,6 +35,7 @@ def write_xls(file_path):
         data5 = pd.read_csv(file_name, encoding='windows-1251', skiprows=119,
                             nrows=16, header=None, decimal=',', sep=";", usecols=[5, 6, 7],names=headers5)
         data = pd.concat([data1, data2, data3, data4, data5], axis=1)
+        data.index=data['f']
         data.to_excel(writer, sheet_name=sheet_name,
                       header=headers, index=False)
         worksheet = writer.sheets[sheet_name]
@@ -42,7 +44,7 @@ def write_xls(file_path):
         chart2 = workbook.add_chart({'type': 'line'})
         chart3 = workbook.add_chart({'type': 'line'})
 
-        for i in range(2, 15, 3):
+        for i in range(2, 18, 3):
             chart1.add_series({
                 'name':       [sheet_name, 0, i],
                 'categories': [sheet_name, 1, 0, 16, 0],
@@ -86,15 +88,31 @@ def write_xls(file_path):
         chart2.set_size({'width': 448, 'height': 300})#320x 226
         worksheet.insert_chart('A49', chart3, {'x_offset': 0, 'y_offset': 0})
         chart3.set_size({'width': 448, 'height': 300})#320x 226
-        alpha_max=data['alfa_75'].max()
-        xmax=data['alfa_75'].idxmax()
-        print(xmax, alpha_max)
     # сортировка листов
-    
     workbook.worksheets_objs.sort(key=lambda x: x.name)
     writer.save()
     writer.close()
 
+def find_resonance(file_path):
+    alpha=['alpha_75','alpha_80','alpha_85','alpha_90','alpha_95']
+    xl=pd.ExcelFile(file_path)
+    sheets=xl.sheet_names
+    for sheet in sheets:
+        print(sheet)
+        df=pd.read_excel(open(file_path, 'rb'),
+              sheet_name=sheet,index_col=0)  
+        alpha_max=[]
+        f_alpha_max=[]
+        for a in [alpha]:
+            alpha_max.append(df[a].max())
+            f_alpha_max.append(df[a].idxmax())
+        print(alpha_max)
+        print(f_alpha_max)
+    wb = load_workbook(file_path)
+    wb.create_sheet('alpha')
+    wb.save(file_path)    
+
 # TODO: максимум альфа, пересение У
 write_xls(total_xls_path)
+find_resonance(total_xls_path)
 print('Done !')
